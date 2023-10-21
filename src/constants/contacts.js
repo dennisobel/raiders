64f3effd32f53153a3f64cab94ca45e4ef9c567f
dennisobel <ogembodenis2016@gmail.com>
2023-10-21 12:32:15 +0300

    remove cell email from address

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,18 +1,18 @@
 import { FaPhone, FaEnvelope, FaMapMarkerAlt } from 'react-icons/fa';
 
 const contacts = [  
-    { 
-        id: 1, 
-        icon: <FaPhone />,
-        title: 'contact',
-        content: [ '+254715179534'],
-    },
-    {
-        id: 2,
-        icon: <FaEnvelope />,
-        title: 'email',
-        content: [ ' awasirovers@gmail.com'],
-    },
+    // { 
+    //     id: 1, 
+    //     icon: <FaPhone />,
+    //     title: 'contact',
+    //     content: [ '+254715179534'],
+    // },
+    // {
+    //     id: 2,
+    //     icon: <FaEnvelope />,
+    //     title: 'email',
+    //     content: [ ' awasirovers@gmail.com'],
+    // },
     {
         id: 3,
         icon: <FaMapMarkerAlt />,
